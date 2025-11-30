e2d1dcc0ab6a91aec4f1bbc7f55b32ddd86771b9
rizvawn <rizvawn@gmail.com>
2025-11-30 19:16:55 +0100

    feat(script): enhance report with block subsystem analysis and improve output formatting

--- investigate.sh
+++ investigate.sh
@@ -11,12 +11,15 @@ report ""
 
 #########################################################
 
-report "[Surface Overview]"
-
 DIR_COUNT_LS="$(ls -l linux/ | grep "^d" | wc -l)"
 FILE_COUNT_LS="$(ls -l linux/ | grep "^-" | wc -l)"
 
-echo "Using ls:   Directories: $DIR_COUNT_LS, Files: $FILE_COUNT_LS"
+echo ""
+echo "[Surface Overview]"
+
+echo "Using ls:   
+Directories: $DIR_COUNT_LS, Files: $FILE_COUNT_LS
+**************"
 
 DIRECTORIES=(linux/*/)
 ALL=(linux/*)
@@ -25,7 +28,46 @@ DIR_COUNT=${#DIRECTORIES[@]}
 ALL_COUNT=${#ALL[@]} 
 FILE_COUNT=$(( ALL_COUNT - DIR_COUNT ))
 
-echo "Using glob: Directories: $DIR_COUNT, Files: $FILE_COUNT"
+echo "Using glob: 
+Directories: $DIR_COUNT, Files: $FILE_COUNT
+***************"
 
+report "[Surface Overview]"
 report "Directories: $DIR_COUNT"
 report "Files: $FILE_COUNT"
+report ""
+
+########################################################
+
+C_FILES_LS="$(ls linux/block/*.c | wc -l)"
+H_FILES_LS="$(ls linux/block/*.h | wc -l)"
+
+echo ""
+echo "[Block Subsystem Analysis]"
+echo "Using ls:   
+Number of implementation files: ${C_FILES_LS}
+Number of headers files: ${H_FILES_LS}
+***************"
+
+C_FILES=(linux/block/*.c)
+H_FILES=(linux/block/*.h)
+
+C_FILES_COUNT=${#C_FILES[@]}
+H_FILES_COUNT=${#H_FILES[@]}
+RATIO=$((C_FILES_COUNT / H_FILES_COUNT))
+
+echo "Using glob:
+Number of implementation files: ${C_FILES_COUNT}
+Number of headers files: ${H_FILES_COUNT}
+The ration between implementation and headers files is ${RATIO}:1
+**************"
+echo ""
+
+report "[Block Subsystem Analysis]"
+report "Number of implementation files: ${C_FILES_COUNT}"
+report "Number of headers files: ${H_FILES_COUNT}"
+report "The ratio between implementation and headers files is ${RATIO}:1"
+report ""
+
+########################################################
+
